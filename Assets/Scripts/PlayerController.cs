6951a5d4224b8cb5a02efd5bf5ce58137bb8c965
Lutas <Fanateshen@gmail.com>
2022-04-21 15:25:30 +0300

    Added Game Over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityMod;
     public bool isOnGround;
+    public bool gameOver;
 
     void Start()
     {
@@ -27,6 +28,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
